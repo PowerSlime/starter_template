8b2f0877d708ae10953bffc216685ec2e8bf6648
PowerSlime <powerslime@ya.ru>
2018-07-13 19:03:20 +0300

    Rewrite gulpfile.js and README.md
    Soon will add webpack support

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,121 @@
 const gulp = require('gulp'),
+	argv = require('yargs').argv,
 	bourbon = require('node-bourbon'),
 	browserSync = require('browser-sync').create(),
 	cleanCss = require('gulp-clean-css'),
-	imagemin = require('gulp-imagemin')
+	del = require('del'),
+	gulpIf = require('gulp-if'),
+	imagemin = require('gulp-imagemin'),
 	imageminJpegRecompress = require('imagemin-jpeg-recompress'),
 	notify = require('gulp-notify'),
+	plumber = require('gulp-plumber'),
 	pug = require('gulp-pug'),
 	rename = require('gulp-rename'),
+	sourcemaps = require('gulp-sourcemaps'),
 	sass = require('gulp-sass');
 
 
-config = {
-	dist_dir: 'dist',
-	src_dir: 'src',
-	run_on_start: ['sass', 'pug', 'move_html_to_dist', 'move_js_to_dist', 'imagemin']
-}
+// Configs
+const config = {
+	runOnBuild: ['html', 'css', 'js', 'imagemin'],
+	path: {
+		source: 'src',
+		dist: 'docs'  // "Docs" folder beacuse we will not need to make "gh-pages" branch
+	},
+
+	// This var is for activating the source maps
+	isDevelopment: argv.N || argv['nosourcemaps'] ? false : true,
+
+	plumber: {
+		errorHandler: notify.onError()
+	}
+};
 
 
-config.images_dist = `${config.dist_dir}/img`;
-config.images_src = `${config.src_dir}/img`;
+const paths = {
+	// We are watching all files for changes but build only those
+	// which are in "build" path
+	build: {
+		css: `${config.path.source}/css/*.{sass,scss}`,
+		html: `${config.path.source}/*.{jade,pug}`,
+		img: `${config.path.source}/img/**/*`,
+		js: `${config.path.source}/js/*`
+	},
+
+	watch: {
+		css: `${config.path.source}/**/*.{sass,scss}`,
+		html: `${config.path.source}/**/*.{jade,pug}`,
+		img: `${config.path.source}/img/**/*`,
+		js: `${config.path.source}/**/*.js`
+	},
+
+	// Part for browser-sync plugin
+	sync: {
+		watch: `${config.path.dist}/**/*.*`,
+	}
+};
 
 
-patterns = {
-	dist: `${config.dist_dir}/**/*`,
-	sass: `${config.src_dir}/**/*.{sass,scss}`,
-	pug: `${config.src_dir}/*.{jade,pug}`,
-	pug_files: `${config.src_dir}/**/*.{jade,pug}`,
-	html: `${config.src_dir}/*.html`,
-	js: `${config.src_dir}/**/*.js`,
-	img: `${config.images_src}/**/*`
-}
-
-
+// Tasks
 gulp.task('browser-sync', () => {
 	browserSync.init({
 		server: {
-			baseDir: config['dist_dir']
+			baseDir: config.path.dist
 		},
 
+		// Disable browser-sync's notification on page
+		// As "Connected to BrowserSync", "Injected main.min.css", etc...
 		notify: false
 	});
+
+	// BrowserSync's watcher
+	browserSync.watch(paths.sync.watch).on('change', browserSync.reload);
 });
 
 
-gulp.task('sass', () => {
-	return gulp.src(patterns.sass)
+gulp.task('clean', () => {
+	return del(config.path.dist);
+});
+
+
+gulp.task('css', () => {
+	return gulp.src(paths.build.css, {base: config.path.source})
+		.pipe(plumber(config.plumber))
+		.pipe(gulpIf(config.isDevelopment, sourcemaps.init()))
 		.pipe(sass({
 			outputStyle: 'compressed',
 			includePaths: bourbon.includePaths
-		}).on("error", notify.onError()))
+		}).on('error', notify.onError()))
 		.pipe(cleanCss({
 			compatibility: 'ie8',
 			level: 2
 		}))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest(`${config.dist_dir}`))
-})
+		.pipe(gulpIf(config.isDevelopment, sourcemaps.write()))
+		.pipe(gulp.dest(config.path.dist));
+});
 
 
-gulp.task('pug', () => {
-	return gulp.src(patterns.pug)
-		.pipe(pug({
-			pretty: '\t'
-		}).on('error', notify.onError()))
-		.pipe(gulp.dest(`${config.dist_dir}`))
-})
+gulp.task('html', () => {
+	return gulp.src(paths.build.html, {base: config.path.source})
+		.pipe(plumber(config.plumber))
+		.pipe(pug())
+		.pipe(gulp.dest(config.path.dist));
+});
 
 
-gulp.task('move_html_to_dist', () => {
-	return gulp.src(patterns.html)
-		.pipe(gulp.dest(`${config.dist_dir}`))
-})
-
-
-gulp.task('move_js_to_dist', () => {
-	return gulp.src(patterns.js)
-		.pipe(gulp.dest(`${config.dist_dir}`))
-})
+gulp.task('js', () => {
+	// In future (v4.0.0) there will be webpack handler
+	// wait a bit for it
+	return gulp.src(paths.build.js, {base: config.path.source})
+		.pipe(plumber(config.plumber))
+		.pipe(gulp.dest(config.path.dist));
+});
 
 
 gulp.task('imagemin', () => {
-	return gulp.src(patterns.img)
+	return gulp.src(paths.build.img, {base: config.path.source})
+		.pipe(plumber(config.plumber))
 		.pipe(imagemin([
 			imagemin.gifsicle({interlaced: true}),
 			imagemin.jpegtran({progressive: true}),
@@ -90,28 +125,22 @@ gulp.task('imagemin', () => {
 				max: 70,
 				quality:'medium'
 			}),
-	    	imagemin.svgo(),
-	    	imagemin.optipng({optimizationLevel: 3})
-	    ]))
-		.pipe(gulp.dest(`${config.images_dist}`))
-})
+			imagemin.svgo(),
+			imagemin.optipng({optimizationLevel: 3})
+		]))
+		.pipe(gulp.dest(config.path.dist));
+});
 
 
-gulp.task('sync', () => {
-	return gulp.src(patterns.dist)
-		.pipe(browserSync.stream());
-})
+// Watchers
+gulp.task('watch', () => {
+	gulp.watch(paths.watch.css, gulp.series('css'));
+	gulp.watch(paths.watch.html, gulp.series('html'));
+	gulp.watch(paths.watch.js, gulp.series('js'));
+	gulp.watch(paths.watch.img, gulp.series('imagemin'));
+});
 
 
-gulp.task('watch', config.run_on_start.concat(['browser-sync']), () => {
-	gulp.watch(patterns.dist, ['sync']);
-	gulp.watch(patterns.sass, ['sass']);
-	gulp.watch(patterns.pug, ['pug']);
-	gulp.watch(patterns.pug_files, ['pug'], null)
-	gulp.watch(patterns.html, ['move_html_to_dist']);
-	gulp.watch(patterns.js, ['move_js_to_dist']);
-	gulp.watch(patterns.img, ['imagemin']);
-})
-
-gulp.task('default', ['watch']);
-gulp.task('build', config.run_on_start)
+// CLI tasks
+gulp.task('build', gulp.series('clean', gulp.parallel(...config.runOnBuild)));
+gulp.task('default', config.isDevelopment ? gulp.series('build', gulp.parallel('watch', 'browser-sync')) : gulp.series('build'));
