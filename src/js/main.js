43702d53f4fcfcb53cc5544f2e4bacd2ac8e650a
Denis <powerslime@ya.ru>
2018-12-09 13:32:34 +0200

    Update styles, removing bootstrap from project

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+// prevent babel from losing this
+// this should be window object
+function init() {
+    this.$ = require("jquery/dist/jquery");
+    this.jQuery = this.$;
+
+    require("svg4everybody/dist/svg4everybody")();
+    require("retinajs/dist/retina");
+
+    $(document).ready(function () {
+
+    });
+}
+
+init.bind(window)();
